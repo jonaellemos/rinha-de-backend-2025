5dedb0c4ca613b6f1546f6891c52e2b2e9151196
Maximillian Arruda <dearrudam@gmail.com>
2025-07-10 08:03:12 -0300

    feat: add PaymentCodec for Redis serialization
    Implement PaymentCodec to handle encoding and decoding
    of Payment objects for Redis, enabling seamless data
    serialization in the application.
    
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

---
+++ code-with-quarkus/src/main/java/org/acme/PaymentCodec.java
@@ -0,0 +1,44 @@
+package org.acme;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.quarkus.redis.datasource.codecs.Codec;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+@ApplicationScoped
+public class PaymentCodec implements Codec {
+
+    private final ObjectMapper objectMapper;
+
+    PaymentCodec(ObjectMapper objectMapper){
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public boolean canHandle(Type clazz) {
+        return clazz.equals(Payment.class);
+    }
+
+    @Override
+    public byte[] encode(Object item) {
+        if (item instanceof Payment) {
+            try {
+                return objectMapper.writeValueAsBytes(item);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return new byte[0];
+    }
+
+    @Override
+    public Object decode(byte[] item) {
+        try {
+            return objectMapper.createParser(item).readValueAs(Payment.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
