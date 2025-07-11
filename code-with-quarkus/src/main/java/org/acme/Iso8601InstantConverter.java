eb7d647b28cc64ab79dc1ab6b51225993261d765
Maximillian Arruda <dearrudam@gmail.com>
2025-07-11 00:26:52 -0300

    feat: implement ObjectMapperCustomizer for Instant
    Enhanced Iso8601InstantConverter to implement ObjectMapperCustomizer,
    allowing automatic registration of the InstantModule with the ObjectMapper.
    
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- code-with-quarkus/src/main/java/org/acme/Iso8601InstantConverter.java
+++ code-with-quarkus/src/main/java/org/acme/Iso8601InstantConverter.java
@@ -4,13 +4,15 @@ import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import io.quarkus.jackson.ObjectMapperCustomizer;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -20,14 +22,15 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.BiFunction;
 
 import static java.util.Optional.ofNullable;
 
 @ApplicationScoped
-public class Iso8601InstantConverter {
+public class Iso8601InstantConverter implements ObjectMapperCustomizer {
 
-    @Inject
-    public Iso8601InstantConverter(ObjectMapper objectMapper) {
+    @Override
+    public void customize(ObjectMapper objectMapper) {
         objectMapper.registerModule(new InstantModule());
     }
 
@@ -45,12 +48,12 @@ public class Iso8601InstantConverter {
         }
 
         @Override
-        public void serialize(Instant value, com.fasterxml.jackson.core.JsonGenerator gen, com.fasterxml.jackson.databind.SerializerProvider provider) throws java.io.IOException {
+        public void serialize(Instant value, com.fasterxml.jackson.core.JsonGenerator gen, com.fasterxml.jackson.databind.SerializerProvider provider) throws java.io.IOException, JacksonException {
             gen.writeString(Iso8601InstantConverter.serialize(value));
         }
     }
 
-    public static class InstantDeserializer extends StdDeserializer<Instant> {
+    public static class InstantDeserializer extends StdDeserializer<Instant> implements BiFunction<ObjectMapper, Type, ObjectReader> {
 
         public InstantDeserializer() {
             super(Instant.class);
@@ -59,6 +62,11 @@ public class Iso8601InstantConverter {
         @Override
         public Instant deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JacksonException {
             return Iso8601InstantConverter.parse(jsonParser.readValueAs(String.class));
+        }
+
+        @Override
+        public ObjectReader apply(ObjectMapper objectMapper, Type type) {
+            return null;
         }
     }
 
@@ -78,34 +86,31 @@ public class Iso8601InstantConverter {
     }
 
     public static Instant parse(String input) {
-        return ofNullable(input)
-                .map(i -> SERIALIZERS.stream()
-                        .map(formatter -> {
-                            try {
-                                TemporalAccessor value = formatter.parseBest(i,
-                                        Instant::from,
-                                        LocalDateTime::from,
-                                        LocalDate::from,
-                                        LocalTime::from);
-                                return switch (value) {
-                                    case Instant instant -> Optional.of(instant);
-                                    case LocalDateTime localDateTime ->
-                                            Optional.of(localDateTime).map(l -> l.toInstant(ZoneOffset.UTC));
-                                    case LocalDate localDate ->
-                                            Optional.of(localDate).map(l -> l.atStartOfDay().toInstant(ZoneOffset.UTC));
-                                    case LocalTime localTime ->
-                                            Optional.of(localTime).map(l -> l.atDate(LocalDate.now()).toInstant(ZoneOffset.UTC));
-                                    default -> Optional.<Instant>empty();
-                                };
-                            } catch (Exception e) {
-                                return Optional.<Instant>empty();
-                            }
-                        })
-                        .filter(Optional::isPresent)
-                        .map(Optional::get)
-                        .findFirst()
-                        .orElse(null)
-                ).orElse(null);
+        return ofNullable(input).flatMap(i -> SERIALIZERS.stream()
+                .map(formatter -> {
+                    try {
+                        TemporalAccessor value = formatter.parseBest(i,
+                                Instant::from,
+                                LocalDateTime::from,
+                                LocalDate::from,
+                                LocalTime::from);
+                        return switch (value) {
+                            case Instant instant -> Optional.of(instant);
+                            case LocalDateTime localDateTime ->
+                                    Optional.of(localDateTime).map(l -> l.toInstant(ZoneOffset.UTC));
+                            case LocalDate localDate ->
+                                    Optional.of(localDate).map(l -> l.atStartOfDay().toInstant(ZoneOffset.UTC));
+                            case LocalTime localTime ->
+                                    Optional.of(localTime).map(l -> l.atDate(LocalDate.now()).toInstant(ZoneOffset.UTC));
+                            default -> Optional.<Instant>empty();
+                        };
+                    } catch (Exception e) {
+                        return Optional.<Instant>empty();
+                    }
+                })
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .findFirst()).orElse(null);
     }
 }
 
