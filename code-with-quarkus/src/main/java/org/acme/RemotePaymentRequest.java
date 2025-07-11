f0afc8c951cbf1ab418038757a460c130e9abfd0
Maximillian Arruda <dearrudam@gmail.com>
2025-07-11 00:29:38 -0300

    feat: enhance RemotePaymentRequest structure
    Added a requestedAt field to the RemotePaymentRequest
    to capture the timestamp of the payment request, defaulting
    to the current time if not provided. Updated the toNewPayment
    method to reflect this change.
    
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- code-with-quarkus/src/main/java/org/acme/RemotePaymentRequest.java
+++ code-with-quarkus/src/main/java/org/acme/RemotePaymentRequest.java
@@ -4,12 +4,23 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.Optional;
 
 public record RemotePaymentRequest(String correlationId,
                                    @JsonFormat(shape = JsonFormat.Shape.NUMBER)
-                                   BigDecimal amount) {
+                                   BigDecimal amount,
+                                   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "UTC")
+                                   Instant requestedAt) {
+
+    public RemotePaymentRequest {
+        requestedAt = Optional.ofNullable(requestedAt).orElse(Instant.now());
+    }
+
+    public Payment toPayment(RemotePaymentName processedBy) {
+        return Payment.of(correlationId, processedBy, amount, requestedAt);
+    }
 
     NewPaymentRequest toNewPayment() {
-        return new NewPaymentRequest(correlationId, amount, Instant.now());
+        return new NewPaymentRequest(correlationId(), amount());
     }
 }
