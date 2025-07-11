8e33a1c9d3288cdd8837c4372a72cd9de60f2e01
Maximillian Arruda <dearrudam@gmail.com>
2025-07-11 00:28:27 -0300

    feat: update payments summary endpoint
    Refactored the payments summary endpoint to handle
    query parameters with default values and added validation
    for null values, improving error handling and user feedback.
    
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- code-with-quarkus/src/main/java/org/acme/PaymentsSummaryResource.java
+++ code-with-quarkus/src/main/java/org/acme/PaymentsSummaryResource.java
@@ -1,15 +1,17 @@
 package org.acme;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.time.Instant;
 
 @Path("/payments-summary")
-@Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class PaymentsSummaryResource {
 
@@ -17,12 +19,21 @@ public class PaymentsSummaryResource {
     Payments payments;
 
     @GET
-    public PaymentsSummary get(@QueryParam("from")
-                               String from,
-                               @QueryParam("to")
-                               String to) {
+    public Response get(@QueryParam("from") @DefaultValue("") String fromStr,
+                        @QueryParam("to") @DefaultValue("") String toStr) {
 
-        return payments.getSummary(Iso8601InstantConverter.parse(from), Iso8601InstantConverter.parse(to));
+        if ("".equals(fromStr) || "".equals(toStr)) {
+            return Response.ok(payments.getSummary(null, null)).build();
+        }
+
+        Instant from = Iso8601InstantConverter.parse(fromStr);
+        Instant to = Iso8601InstantConverter.parse(toStr);
+
+        if (from == null || to == null) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("from and to cannot be null.").build();
+        }
+
+        return Response.ok(payments.getSummary(from, to)).build();
     }
 
 }
